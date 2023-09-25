73ab3c6c345b9f9139d386cab9189e13c8e187a1
Dimitrios Gretsistas <dimitriosgretsistas@gmail.com>
2023-09-25 15:50:05 +0200

    client side

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { createRoutesFromChildren } from "react-router-dom";
+import "./App.css";
+import { Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ProjectListPage from "./pages/ProjectListPage";
+import ProjectDetailsPage from "./pages/ProjectDetailsPage";      //  <== IMPORT
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectListPage />} />
+        <Route path="/projects/:projectId" element={<ProjectDetailsPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
